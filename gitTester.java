1d6764f7a889ac1841e53d126904901439332def
Hugh Cheng <hcheng1@hwemail.com>
2024-09-26 02:14:08 -0700

    Git Project Part 2
    Trees are now makeable

--- gitTester.java
+++ gitTester.java
@@ -3,36 +3,47 @@ import java.io.IOException;
 
 public class gitTester {
     public static void main(String[] args) throws IOException {
-        checkAndDelete();
+        wipe(new File("git"));
         Git repo = new Git();
         File testFile = new File("testfile.txt");
-        repo.makeBlob(testFile);
+        repo.makeBlob(testFile.getPath());
+        File testDir = new File("testDir");
+        repo.makeBlob(testDir.getPath());
     }
 
-    //checks if all correct files exist in their propper locations and then deletes them after
-    public static void checkAndDelete() throws IOException{
+    // recursively remove a file
+    public static void wipe(File file) throws IOException {
+        if (file.exists() && file.isDirectory())
+            for (File f : file.listFiles())
+                wipe(f);
+        file.delete();
+    }
+
+    // checks if all correct files exist in their propper locations and then deletes
+    // them after
+
+    public static void checkAndDelete() throws IOException {
         File gitDir = new File("git");
 
         File objectsDir = new File("git/objects");
 
         File indexFile = new File("git/index");
 
-        if (gitDir.exists() && objectsDir.exists() && indexFile.exists()){
+        if (gitDir.exists() && objectsDir.exists() && indexFile.exists()) {
             System.out.println("Git repository exists");
-        }
-        else{
-            if (!gitDir.exists()){
-                System.out.println("git directory doesnt exist");  
+        } else {
+            if (!gitDir.exists()) {
+                System.out.println("git directory doesnt exist");
             }
-            if (!objectsDir.exists()){
-                System.out.println("objects directory doesnt exist");  
+            if (!objectsDir.exists()) {
+                System.out.println("objects directory doesnt exist");
             }
-            if (!indexFile.exists()){
-                System.out.println("index doesnt exist"); 
+            if (!indexFile.exists()) {
+                System.out.println("index doesnt exist");
             }
         }
 
-        System.out.println("now deleting:");  
+        System.out.println("now deleting:");
 
         objectsDir.delete();
         indexFile.delete();
